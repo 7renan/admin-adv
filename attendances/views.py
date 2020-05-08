c4248ec356ca0d1ff694417d34f3f210a267b187
Renan Lima <renan@lima>
2020-05-08 10:51:48 -0300

    list attendance

--- attendances/views.py
+++ attendances/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 
+# models
+from attendances.models import Attendance
 
-class AttendanceList(TemplateView):
+
+class AttendanceList(ListView):
     template_name = 'attendance/attendance_list.html'
+    model = Attendance
+    paginate_by = 50
+    context_object_name = 'attendances'
